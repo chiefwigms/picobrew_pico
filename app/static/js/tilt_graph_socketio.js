c0e9fe6520303e6ebb68fe5101be7217745fcc08
David Wilt <wiltdavi@gmail.com>
2021-11-29 15:57:03 -0500

    Fixed Formatting
    Fixed the formatting spacing to match tilt_graph.js

--- app/static/js/tilt_graph_socketio.js
+++ app/static/js/tilt_graph_socketio.js
@@ -50,14 +50,14 @@ Highcharts.chart(graph_data.chart_id, {
 
   tooltip: {
     formatter: function() {
-        var s = '<b>'+ Highcharts.dateFormat('%A, %b %e %k:%M:%S.%L', // Friday, Jan ## ##:##:##.####
-          new Date(this.x)) +'</b>';
+      var s = '<b>'+ Highcharts.dateFormat('%A, %b %e %k:%M:%S.%L', // Friday, Jan ## ##:##:##.####
+      new Date(this.x)) +'</b>';
 
-        $.each(this.points, function(i, point) {
-            s += '<br/><span style="color:' + point.color + '">\u25CF</span> ' + point.series.name + ': ' + point.y;
-        });
+      $.each(this.points, function(i, point) {
+          s += '<br/><span style="color:' + point.color + '">\u25CF</span> ' + point.series.name + ': ' + point.y;
+      });
 
-        return s;
+      return s;
     },
     shared: true
   },
@@ -72,50 +72,49 @@ Highcharts.chart(graph_data.chart_id, {
     },
   },
 
-  yAxis: [
-    {
-      title: {
-        text: 'Temperature (F)'
-      },
-	  
-	  tickPositioner: function () {
-        var positions = [],
-        tick = Math.floor(this.dataMin),
-        increment = Math.ceil((this.dataMax - this.dataMin) / 6);
+  yAxis: [{
+    title: {
+      text: 'Temperature (F)'
+    },
 
-        if (this.dataMax !== null && this.dataMin !== null) {
-          for (tick; tick - increment <= this.dataMax; tick += increment) {
-            positions.push(tick);
-          }
-        }
-		TickAmountValue = positions.length;
-        return positions;
-	  },
-    }, {
-      title: {
-        text: 'Specific Gravity'
-      },
-	  
-	  labels: {
-        format: '{value:,.3f}'
-	  },
-	  
-	  tickPositioner: function () {
-        var positions = [],
-        maxTick = this.dataMax * 1000,
-        step = (maxTick - this.dataMin * 1000) / (TickAmountValue - 1),
-        tick = this.dataMin * 1000 - 1;
+    tickPositioner: function () {
+      var positions = [],
+      tick = Math.floor(this.dataMin),
+      increment = Math.ceil((this.dataMax - this.dataMin) / 6);
 
-        if (this.dataMax !== null && this.dataMin !== null) {
-          while (tick <= maxTick) {
-            positions.push(tick/1000);
-            tick += step;
-          }
+      if (this.dataMax !== null && this.dataMin !== null) {
+        for (tick; tick - increment <= this.dataMax; tick += increment) {
+          positions.push(tick);
         }
-        return positions;
-	  },
-	  
-      opposite: true
+      }
+      TickAmountValue = positions.length;
+      return positions;
+    },
+  }, {
+    title: {
+      text: 'Specific Gravity'
+    },
+
+    labels: {
+      format: '{value:,.3f}'
+    },
+
+    tickPositioner: function () {
+      var positions = [],
+      maxTick = this.dataMax * 1000,
+      step = (maxTick - this.dataMin * 1000) / (TickAmountValue - 1),
+      tick = this.dataMin * 1000 - 1;
+
+      if (this.dataMax !== null && this.dataMin !== null) {
+        while (tick <= maxTick) {
+          positions.push(tick/1000);
+          tick += step;
+        }
+      }
+      return positions;
+    },
+
+    opposite: true
   }],
   
   series: graph_data.series,
